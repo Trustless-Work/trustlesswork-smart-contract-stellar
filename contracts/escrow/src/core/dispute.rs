16b1899be8e387417888dd0416f8e2f3b5dfe539
armandocodecr <totovip1464@gmail.com>
2026-08-11 21:09:44 -0600

    fix: add reentrancy guard to resolve_dispute

--- contracts/escrow/src/core/dispute.rs
+++ contracts/escrow/src/core/dispute.rs
@@ -88,6 +88,11 @@ impl DisputeManager {
         milestone_indices: Vec<u32>,
         distributions: Map<Address, i128>,
     ) -> Result<(Escrow, StandardFeeResult, Vec<(Address, i128)>), EscrowError> {
+        if e.storage().persistent().has(&DataKey::Reentrancy) {
+            return Err(EscrowError::FlagsMustBeFalse);
+        }
+        e.storage().persistent().set(&DataKey::Reentrancy, &true);
+
         let mut escrow = EscrowManager::get_escrow(e)?;
         let contract_address = e.current_contract_address();
 
@@ -137,6 +142,8 @@ impl DisputeManager {
             &distributions,
             total,
         )?;
+
+        e.storage().persistent().remove(&DataKey::Reentrancy);
 
         Ok((escrow, fee_result, net_dists))
     }
